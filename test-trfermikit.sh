0e9c2c53bb3145c726bdce7c55b40e182253feff
Peter McHale <peter.thomas.mchale@gmail.com>
2020-11-20 14:45:02 -0700

    make-calls

--- test-trfermikit.sh
+++ test-trfermikit.sh
@@ -5,15 +5,15 @@ set -o pipefail
 set -o nounset
 set -o noclobber
 
-set -o xtrace
-# Must use single quote to prevent variable expansion.
-# For example, if double quotes were used, ${LINENO} would take on the value of the current line,
-# instead of its value when PS4 is used later in the script
-# https://stackoverflow.com/a/6697845/6674256
-# ${FOO:+val}    val if $FOO is set
-# ${FOO[0]}   element #0 of the FOO array
-# https://www.gnu.org/software/bash/manual/html_node/Bash-Variables.html
-PS4='+ (${BASH_SOURCE[0]##*/} @ ${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+# set -o xtrace
+# # Must use single quote to prevent variable expansion.
+# # For example, if double quotes were used, ${LINENO} would take on the value of the current line,
+# # instead of its value when PS4 is used later in the script
+# # https://stackoverflow.com/a/6697845/6674256
+# # ${FOO:+val}    val if $FOO is set
+# # ${FOO[0]}   element #0 of the FOO array
+# # https://www.gnu.org/software/bash/manual/html_node/Bash-Variables.html
+# PS4='+ (${BASH_SOURCE[0]##*/} @ ${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 
 population="CHS"
 sample="HG00514"
