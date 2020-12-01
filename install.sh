61917c6ca9e119a95d643e35ba25ceafe63993d2
Peter McHale <peter.thomas.mchale@gmail.com>
2020-12-01 11:13:59 -0700

    dependencies

--- install.sh
+++ install.sh
@@ -6,29 +6,29 @@ PS4='+ (${BASH_SOURCE[0]##*/} @ ${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 
 ########################## 
 
+kernel=$(uname --kernel-name)
+machine=$(uname --machine)
+
+if [[ ${machine} != 'x86_64' || ${kernel} != 'Linux'* ]]; then
+    bash utilities/error.sh "not Linux x86_64"
+    exit 1
+fi 
+
+########################## 
+
 if ! which conda; then 
   bash utilities/error.sh "please install conda"
   exit 1
 fi 
 
-conda create --force --name trfermikit python=3.8 colored-traceback colorama cyvcf2
+conda create --name trfermikit python=3.8 colorama cyvcf2
+source activate trfermikit 
+pip install colored-traceback 
 
 ########################## 
 
-kernel=$(uname --kernel-name)
-machine=$(uname --machine)
 
-if [[ ${machine} == 'x86_64' ]]; then
-  if [[ ${kernel} == 'Linux'* ]]; then
+
     # download bedtools, samtools, minimap2, jq, bcftools, etc and install into $PWD/dependencies/bin
     # 	— (reference download_install_*.sh files
-    echo "download bedtools, samtools, minimap2, jq, bcftools, etc and install into $PWD/dependencies/bin"
-  else
-    echo 'not Linux'
-    exit 1
-  fi
-else
-  echo 'not x86_64'
-  exit 1
-fi
 
