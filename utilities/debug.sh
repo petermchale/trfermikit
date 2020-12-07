719dcc292a51826d0ac334638fe86bb5ca107e2f
Peter McHale <peter.thomas.mchale@gmail.com>
2020-12-07 15:51:16 -0700

    min repeat length

--- utilities/debug.sh
+++ utilities/debug.sh
@@ -2,12 +2,10 @@
 
 # TODO: eliminate all xtrace commands in other scripts in favor of the following, 
 # which separates xtrace output from non-xtrace output that appears on STDERR
-if [[ -v TRACE ]]; then
-  bash ${root}/utilities/info.sh "Run TRACE mode"
-  exec 4>./xtrace.out
-  BASH_XTRACEFD=4
-  set -o xtrace # same as set -x
-fi
+bash ${root}/utilities/info.sh "Run TRACE mode"
+exec 4>${output}/xtrace.out
+BASH_XTRACEFD=4
+set -o xtrace # same as set -x
 
 log_error () { 
   bash ${root}/utilities/error.sh "${BASH_COMMAND} failed with error code $?"
