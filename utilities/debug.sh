57120bbaae59ad83776b055062aa7bb19763f836
Peter McHale <peter.thomas.mchale@gmail.com>
2020-12-07 14:36:28 -0700

    min repeat length

--- utilities/debug.sh
+++ utilities/debug.sh
@@ -2,9 +2,6 @@
 
 # https://www.shell-tips.com/bash/debug-script/
 
-echo "root is: $root"
-exit 1 
-
 log_error () { 
   bash ${root}/utilities/error.sh "${BASH_COMMAND} failed with error code $?"
 }
