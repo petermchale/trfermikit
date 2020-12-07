7bc9298a2522a3565bb2b7ff0feef26e5d3f62c3
Peter McHale <peter.thomas.mchale@gmail.com>
2020-12-07 11:36:11 -0700

    min repeat length

--- utilities/color_text.py
+++ utilities/color_text.py
@@ -2,7 +2,7 @@ from colorama import init as colorama_init
 from colorama import Fore, Style
 import sys
 
-colorama_init()
+colorama_init(convert=True)
 
 def error(text):
   print(Fore.RED + text + Style.RESET_ALL, file=sys.stderr)
