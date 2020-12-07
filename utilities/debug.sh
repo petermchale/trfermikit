17b679c021a512e375bd543623d6255967a9c7e4
Peter McHale <peter.thomas.mchale@gmail.com>
2020-12-07 15:39:41 -0700

    min repeat length

--- utilities/debug.sh
+++ utilities/debug.sh
@@ -1,6 +1,7 @@
 # https://www.shell-tips.com/bash/debug-script/ 
 
-# TODO: eliminate all xtrace commands in other scripts in favor of the following: 
+# TODO: eliminate all xtrace commands in other scripts in favor of the following, 
+# which separates xtrace output from non-xtrace output that appears on STDERR
 # if [[ -v TRACE ]]; then
 #   echo "Run TRACE mode"
 #   exec 4>./xtrace.out
