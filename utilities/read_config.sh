42c3d934aefb82c7d4959d5dd348f330a55a4cb9
Peter McHale <peter.thomas.mchale@gmail.com>
2020-12-08 12:24:16 -0700

    config

--- utilities/read_config.sh
+++ utilities/read_config.sh
@@ -8,5 +8,9 @@ key1=$3
 key2=$4 
 
 echo $(${root}/bin/jq \
-        --raw-output --arg key1 $key1 --arg key2 $key2 '.[$key1][$key2]' ${output}/config.json)
+        --raw-output \
+        --arg key1 $key1 \
+        --arg key2 $key2 \
+        '.[$key1][$key2]' \
+        ${output}/config.json)
 
