61e4e23e8130796d667f10a8e428ce4d94884331
Peter McHale <peter.thomas.mchale@gmail.com>
2020-12-08 12:09:42 -0700

    config

--- utilities/update_config.sh
+++ utilities/update_config.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -o xtrace 
+set -o errexit 
 
 root=$1 
 output=$2
@@ -12,7 +13,7 @@ ${root}/bin/jq \
     --arg key1 $key1 \
     --arg key2 $key2 \
     --arg value $value \
-    '.[$key1].[$key2] = $value' \
+    '.[$key1][$key2] = $value' \
     $output/config.json \
   > $output/config.tmp.json
 mv $output/config.tmp.json $output/config.json
