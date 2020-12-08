463339c0e91732e340e8260a7bde7d771d3308ef
Peter McHale <peter.thomas.mchale@gmail.com>
2020-12-08 11:34:44 -0700

    config

--- utilities/fetch_value.sh
+++ utilities/fetch_value.sh
@@ -6,7 +6,5 @@ root=$1
 key1=$2 
 key2=$3 
 
-fetch_value () { 
-  echo $(${root}/bin/jq --raw-output --arg key1 $key1 --arg key2 $key2 '.[$key1][$key2]' ${root}/config.json)
-}
+echo $(${root}/bin/jq --raw-output --arg key1 $key1 --arg key2 $key2 '.[$key1][$key2]' ${root}/config.json)
 
