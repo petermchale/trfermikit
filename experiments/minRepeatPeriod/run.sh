7bf2297499b449431723c581c155078b8569c3d7
Peter McHale <peter.thomas.mchale@gmail.com>
2020-12-08 17:21:03 -0700

    period experiments

--- experiments/minRepeatPeriod/run.sh
+++ experiments/minRepeatPeriod/run.sh
@@ -13,45 +13,43 @@ set -o xtrace
 # https://www.gnu.org/software/bash/manual/html_node/Bash-Variables.html
 PS4='+ (${BASH_SOURCE[0]##*/} @ ${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 
+genome_build="hg38" # or "hg19"
+reference="/scratch/ucgd/lustre-work/quinlan/u6018199/chaisson_2019/reference/GRCh38_full_analysis_set_plus_decoy_hla"
+
 population="CHS"
 sample="HG00514"
-
-genome_build="hg38" # or "hg19"
-
-# *.fa :
-reference="/scratch/ucgd/lustre-work/quinlan/u6018199/chaisson_2019/reference/GRCh38_full_analysis_set_plus_decoy_hla"
-# *.cram, *.cram.crai : 
 alignments="/scratch/ucgd/lustre-work/quinlan/u6018199/chaisson_2019/illumina_crams/ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/hgsv_sv_discovery/data/${population}/${sample}/high_cov_alignment/${sample}.alt_bwamem_GRCh38DH.20150715.${population}.high_coverage"
-# *.bed.gz :
-functional_regions="/scratch/ucgd/lustre-work/quinlan/u6018199/chaisson_2019/genes/Homo_sapiens.GRCh38.99" 
 
 min_repeat_length="100"
-
 number_threads="16"
 svtype="DEL"
 
-trfermikit_path="/scratch/ucgd/lustre-work/quinlan/u6018199/chaisson_2019/analysis/locally_assemble_short_reads/trfermikit"
-output="${trfermikit_path}/data/${sample}.${svtype}"
-PATH="${trfermikit_path}:$PATH"
+root="/scratch/ucgd/lustre-work/quinlan/u6018199/chaisson_2019/analysis/locally_assemble_short_reads/trfermikit"
+PATH="${root}:$PATH"
 
-# the arguments --min-repeat-length and --functional-regions are optional 
-# only svtype==DEL is currently supported
-trfermikit \
-  --genome-build ${genome_build} \
-  --output ${output} \
-  --reference ${reference} \
-  --threads ${number_threads} \
-  --svtype ${svtype} \
-  --alignments ${alignments} \
-  --min-repeat-length ${min_repeat_length} \
-  --functional-regions ${functional_regions} 
+for minRepeatPeriod in 0 6; do 
+  output="${root}/experiments/minRepeatPeriod/minRepeatPeriod=${minRepeatPeriod}"
 
-bash ${trfermikit_path}/evaluate-calls/evaluate.sh \
+  cp ${root}/config.core.json ${output}/config.json
+  ${root}/utilities/update_config.sh ${root} ${output} makeRegions minRepeatPeriod ${minRepeatPeriod}
+  exit 1
+
+  trfermikit \
+    --genome-build ${genome_build} \
     --output ${output} \
-    --threads ${number_threads} \
     --reference ${reference} \
-    --population ${population} \
-    --sample ${sample} \
+    --threads ${number_threads} \
     --svtype ${svtype} \
-    --root ${trfermikit_path} \
-  2> ${output}/evaluate-calls.log 
+    --alignments ${alignments} \
+    --min-repeat-length ${min_repeat_length} \
+
+  bash ${root}/evaluate-calls/evaluate.sh \
+      --output ${output} \
+      --threads ${number_threads} \
+      --reference ${reference} \
+      --population ${population} \
+      --sample ${sample} \
+      --svtype ${svtype} \
+      --root ${root} \
+    2> ${output}/evaluate-calls.log 
+done
