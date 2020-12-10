dd39b90961b43ffeddf8ce467dc8ff104fb497c9
petermchale <peter.thomas.mchale@gmail.com>
2020-12-10 15:25:08 -0700

    minRepeatLength experiments

---
+++ experiments/minRepeatLength/run.sh
@@ -0,0 +1,26 @@
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o noclobber
+
+set -o xtrace
+# Must use single quote to prevent variable expansion.
+# For example, if double quotes were used, ${LINENO} would take on the value of the current line,
+# instead of its value when PS4 is used later in the script
+# https://stackoverflow.com/a/6697845/6674256
+# ${FOO:+val}    val if $FOO is set
+# ${FOO[0]}   element #0 of the FOO array
+# https://www.gnu.org/software/bash/manual/html_node/Bash-Variables.html
+PS4='+ (${BASH_SOURCE[0]##*/} @ ${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+
+root="/scratch/ucgd/lustre-work/quinlan/u6018199/chaisson_2019/analysis/locally_assemble_short_reads/trfermikit"
+
+for minRepeatLength in 0 50 100; do 
+  output="${root}/experiments/minRepeatLength/data/minRepeatLength=${minRepeatLength}"
+  mkdir --parents ${output}
+
+  sbatch \
+    --job-name="minRepeatLength=${minRepeatLength}" \
+    --output="${output}/slurm.%j.log" \
+    ${root}/experiments/minRepeatLength/run_trfermikit_and_evaluate_calls.sh ${root} ${output} ${minRepeatLength}
+done
