cf60f8b1e23aeff1cc11eee36e5352bfd8525376
petermchale <peter.thomas.mchale@gmail.com>
2020-12-14 13:42:09 -0700

    run ROC expts

--- experiments/ROC/run.sh
+++ experiments/ROC/run.sh
@@ -1,28 +1,55 @@
-set -o errexit
+#set -o errexit
 set -o pipefail
 set -o nounset
 # set -o noclobber
 
-set -o xtrace
-# Must use single quote to prevent variable expansion.
-# For example, if double quotes were used, ${LINENO} would take on the value of the current line,
-# instead of its value when PS4 is used later in the script
-# https://stackoverflow.com/a/6697845/6674256
-# ${FOO:+val}    val if $FOO is set
-# ${FOO[0]}   element #0 of the FOO array
-# https://www.gnu.org/software/bash/manual/html_node/Bash-Variables.html
-PS4='+ (${BASH_SOURCE[0]##*/} @ ${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+# set -o xtrace
+# # Must use single quote to prevent variable expansion.
+# # For example, if double quotes were used, ${LINENO} would take on the value of the current line,
+# # instead of its value when PS4 is used later in the script
+# # https://stackoverflow.com/a/6697845/6674256
+# # ${FOO:+val}    val if $FOO is set
+# # ${FOO[0]}   element #0 of the FOO array
+# # https://www.gnu.org/software/bash/manual/html_node/Bash-Variables.html
+# PS4='+ (${BASH_SOURCE[0]##*/} @ ${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 
 root="/scratch/ucgd/lustre-work/quinlan/u6018199/chaisson_2019/analysis/locally_assemble_short_reads/trfermikit"
 
-for minRepeatPeriod in 0 6; do 
-  output="${root}/experiments/minRepeatPeriod/data/minRepeatPeriod=${minRepeatPeriod}"
-  mkdir --parents ${output}
-  cp ${root}/config.core.json ${output}/config.json
-  ${root}/utilities/update_config.sh ${root} ${output} makeRegions minRepeatPeriod ${minRepeatPeriod}
+job_count=0 
 
-  sbatch \
-    --job-name="minRepeatPeriod=${minRepeatPeriod}" \
-    --output="${output}/slurm.%j.log" \
-    ${root}/experiments/minRepeatPeriod/run_trfermikit_and_evaluate_calls.sh ${root} ${output}
+for minCoverage in 0 5 10; do # trfermikit is "0"
+  d1="minCoverage=${minCoverage}"
+  for gapOpenPenalties in 3,15 6,26 10,35; do # asm10 is "16,41"; trfermikit is "6,26"
+    d2="gapOpenPenalties=${gapOpenPenalties}"
+    for singleBaseMatchReward in 10; do # asm10 is "1"; trfermikit is "10"
+      # d3="singleBaseMatchReward=${singleBaseMatchReward}"
+      for singleBaseMismatchPenalty in 12; do # asm10 is "9"; trfermikit is "12"
+        # d4="singleBaseMismatchPenalty=${singleBaseMismatchPenalty}"
+        for minUnitigMappingQuality in 0 10; do # trfermikit is "0" 
+          d5="minUnitigMappingQuality=${minUnitigMappingQuality}"
+          for minUnitigBlockLength in 15 25 40; do # trfermikit is "25" 
+            d6="minUnitigBlockLength=${minUnitigBlockLength}"
+            experiment="${d1};${d2};${d5};${d6}"
+            output="${root}/experiments/ROC/data/${experiment}"
+            mkdir --parents ${output}
+            cp ${root}/config.core.json ${output}/config.json
+            ${root}/utilities/update_config.sh ${root} ${output} makeRegions minCoverage ${minCoverage}
+            ${root}/utilities/update_config.sh ${root} ${output} makeCalls gapOpenPenalties ${gapOpenPenalties}
+            ${root}/utilities/update_config.sh ${root} ${output} makeCalls singleBaseMatchReward ${singleBaseMatchReward}
+            ${root}/utilities/update_config.sh ${root} ${output} makeCalls singleBaseMismatchPenalty ${singleBaseMismatchPenalty}
+            ${root}/utilities/update_config.sh ${root} ${output} filterCalls minUnitigMappingQuality ${minUnitigMappingQuality}
+            ${root}/utilities/update_config.sh ${root} ${output} filterCalls minUnitigBlockLength ${minUnitigBlockLength}
+            
+            sbatch \
+              --job-name="${experiment}" \
+              --output="${output}/slurm.%j.log" \
+              ${root}/experiments/ROC/run_trfermikit_and_evaluate_calls.sh ${root} ${output}
+            ((job_count++))          
+          done
+        done
+      done
+    done
+  done
 done
+ 
+bash ${root}/utilities/info.sh "number of jobs submitted: $job_count"
--- utilities/update_config.sh
+++ utilities/update_config.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-set -o xtrace 
 set -o errexit 
+set -o nounset
 
 root=$1 
 output=$2
 key1=$3
 key2=$4 
 value=$5
-
+ 
+set -o xtrace
 ${root}/bin/jq \
     --arg key1 $key1 \
     --arg key2 $key2 \
@@ -16,4 +17,6 @@ ${root}/bin/jq \
     '.[$key1][$key2] = $value' \
     $output/config.json \
   > $output/config.tmp.json
+set +o xtrace 
+
 mv $output/config.tmp.json $output/config.json
