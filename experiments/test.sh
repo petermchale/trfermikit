ae1b086be4f1819c6c631b603915d77c69bb9e73
Peter McHale <peter.thomas.mchale@gmail.com>
2020-12-16 16:15:54 -0700

    test get_sv_length refactor

--- experiments/test.sh
+++ experiments/test.sh
@@ -1,7 +1,7 @@
 root="/scratch/ucgd/lustre-work/quinlan/u6018199/chaisson_2019/analysis/locally_assemble_short_reads/trfermikit"
 svtype="DEL"
 output="minCoverage_gapOpenPenalties_minUnitigMappingQuality_minUnitigBlockLength/data/minCoverage=0_gapOpenPenalties=5,20_minUnitigMappingQuality=10_minUnitigBlockLength=25"
-parameters="${output}/config.json"
+parameters="${output}/config"
 tr_fermikit_calls="${output}/fermikit.raw"
 
 # https://stackoverflow.com/a/43476575/6674256
