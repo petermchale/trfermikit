57447bd19382c15ef1f94b3ec1b9ff531031f159
petermchale <peter.thomas.mchale@gmail.com>
2020-12-16 17:57:45 -0700

    minSVSize expts

---
+++ experiments/minSVSize/run.sh
@@ -0,0 +1,33 @@
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o noclobber
+
+set -o xtrace
+# Must use single quote to prevent variable expansion.
+# For example, if double quotes were used, ${LINENO} would take on the value of the current line,
+# instead of its value when PS4 is used later in the script
+# https://stackoverflow.com/a/6697845/6674256
+# ${FOO:+val}    val if $FOO is set
+# ${FOO[0]}   element #0 of the FOO array
+# https://www.gnu.org/software/bash/manual/html_node/Bash-Variables.html
+PS4='+ (${BASH_SOURCE[0]##*/} @ ${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+
+root="/scratch/ucgd/lustre-work/quinlan/u6018199/chaisson_2019/analysis/locally_assemble_short_reads/trfermikit"
+
+genome_build="hg38" # or "hg19"
+
+for minSVSize in 20 30 40 50 75 100; do 
+  output="${root}/experiments/minSVSize/data/minSVSize=${minSVSize}"
+  mkdir --parents ${output}
+
+  cp ${root}/config.core.json ${output}/config.json
+  ${root}/utilities/update_config.sh ${root} ${output} filterCalls minSVSize ${minSVSize}
+
+  ln -s ${root}/experiments/repeats.${genome_build}.tab.gz ${output} 
+
+  sbatch \
+    --job-name="minSVSize=${minSVSize}" \
+    --output="${output}/slurm.%j.log" \
+    ${root}/experiments/minSVSize/run_trfermikit_and_evaluate_calls.sh ${root} ${output} 
+done
