54d660ad2c1bd05ac49192d3ff49af45a6d0d4dd
petermchale <peter.thomas.mchale@gmail.com>
2020-12-23 18:02:12 -0700

    minSVSize notebook for INSs

--- experiments/fetch_slurm_job_ids.sh
+++ experiments/fetch_slurm_job_ids.sh
@@ -1,10 +1,8 @@
 experiment=$1 
 
-rm --force ${experiment}/data/job_ids.txt 
-
 for log in ${experiment}/data/*/slurm*log; do 
   log=$(basename $log)
   log=${log#slurm.} 
   job_id=${log%.log}
-  echo $job_id >> ${experiment}/data/job_ids.txt
+  echo $job_id 
 done
