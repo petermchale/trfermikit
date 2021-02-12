c1285a62c144502e1e53e1ce8dff402f838ff8ee
petermchale <peter.thomas.mchale@gmail.com>
2021-02-12 15:52:29 -0700

    test

--- test/create_mini_cram.sh
+++ test/create_mini_cram.sh
@@ -1,3 +1,17 @@
+set -o errexit
+set -o pipefail
+# set -o noclobber
+
+set -o xtrace
+# Must use single quote to prevent variable expansion.
+# For example, if double quotes were used, ${LINENO} would take on the value of the current line,
+# instead of its value when PS4 is used later in the script
+# https://stackoverflow.com/a/6697845/6674256
+# ${FOO:+val}    val if $FOO is set
+# ${FOO[0]}   element #0 of the FOO array
+# https://www.gnu.org/software/bash/manual/html_node/Bash-Variables.html
+PS4='+ (${BASH_SOURCE[0]##*/} @ ${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+
 reference="/scratch/ucgd/lustre-work/quinlan/u6018199/chaisson_2019/reference/GRCh38_full_analysis_set_plus_decoy_hla"
 
 population="CHS"
@@ -6,4 +20,5 @@ alignments="/scratch/ucgd/lustre-work/quinlan/u6018199/chaisson_2019/illumina_cr
 
 region="chr1:500000-1000000" 
 
-samtools view -C --reference ${reference}.fa ${alignments}.cram ${region}  
+samtools view -C --reference ${reference}.fa ${alignments}.cram ${region} > alignments.cram
+samtools index alignments.cram
