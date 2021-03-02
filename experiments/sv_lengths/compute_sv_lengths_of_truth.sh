14e3758661c4376881d61f321325edc9a6e13fa6
Peter McHale <peter.thomas.mchale@gmail.com>
2021-03-02 10:10:25 -0700

    sv lengths

--- experiments/sv_lengths/compute_sv_lengths_of_truth.sh
+++ experiments/sv_lengths/compute_sv_lengths_of_truth.sh
@@ -25,5 +25,5 @@ for regions in all genes exons_UTRs; do
   fi
   output="/scratch/ucgd/lustre-work/quinlan/u6018199/chaisson_2019/analysis/locally_assemble_short_reads/trfermikit/experiments/${directory}/singleBaseMatchReward_singleBaseMismatchPenalty_gapOpenPenalties_gapExtensionPenalties/data/gapExtensionPenalties=1,0_gapOpenPenalties=16,41_singleBaseMatchReward=10_singleBaseMismatchPenalty=12"
   bash compute_sv_lengths_of_truth_on_regions.sh --output ${output} \
-    > sv_lengths.${regions}.txt
+    > sv_lengths.${regions}.csv
 done 
