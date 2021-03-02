3b26d40bf8a4bd1aff42fa792015b30e944a1902
Peter McHale <peter.thomas.mchale@gmail.com>
2021-03-02 12:35:17 -0700

    sv lengths

--- experiments/sv_lengths/compute_sv_lengths.sh
+++ experiments/sv_lengths/compute_sv_lengths.sh
@@ -18,22 +18,23 @@ set -o xtrace
 PS4='+ (${BASH_SOURCE[0]##*/} @ ${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 
 svtype="DEL"
-calls_name="pacbio"
 
-for regions in all_regions regions_intersecting_genes regions_intersecting_exons_and_UTRs; do
-  if [[ ${regions} == "all_regions" ]]; then 
-    directory="INS"
-  elif [[ ${regions} == "regions_intersecting_genes" ]]; then
-    directory="genes"
-  elif [[ ${regions} == "regions_intersecting_exons_and_UTRs" ]]; then
-    directory="exons_UTRs"
-  else
-    echo -e "${RED}${regions} is invalid${NO_COLOR}" >&2
-  fi
-  output="/scratch/ucgd/lustre-work/quinlan/u6018199/chaisson_2019/analysis/locally_assemble_short_reads/trfermikit/experiments/${directory}/singleBaseMatchReward_singleBaseMismatchPenalty_gapOpenPenalties_gapExtensionPenalties/data/gapExtensionPenalties=1,0_gapOpenPenalties=16,41_singleBaseMatchReward=10_singleBaseMismatchPenalty=12"
-  bash compute_sv_lengths_on_regions.sh \
-      --output ${output} \
-      --svtype ${svtype} \
-      --calls-name ${calls_name} \
-    > sv_lengths.${regions}.${svtype}.${calls_name}.csv
+for calls_name in pacbio trfermikit manta; do
+  for regions in all_regions regions_intersecting_genes regions_intersecting_exons_and_UTRs; do
+    if [[ ${regions} == "all_regions" ]]; then 
+      directory="INS"
+    elif [[ ${regions} == "regions_intersecting_genes" ]]; then
+      directory="genes"
+    elif [[ ${regions} == "regions_intersecting_exons_and_UTRs" ]]; then
+      directory="exons_UTRs"
+    else
+      echo -e "${RED}${regions} is invalid${NO_COLOR}" >&2
+    fi
+    output="/scratch/ucgd/lustre-work/quinlan/u6018199/chaisson_2019/analysis/locally_assemble_short_reads/trfermikit/experiments/${directory}/singleBaseMatchReward_singleBaseMismatchPenalty_gapOpenPenalties_gapExtensionPenalties/data/gapExtensionPenalties=1,0_gapOpenPenalties=16,41_singleBaseMatchReward=10_singleBaseMismatchPenalty=12"
+    bash compute_sv_lengths_on_regions.sh \
+        --output ${output} \
+        --svtype ${svtype} \
+        --calls-name ${calls_name} \
+      > sv_lengths.${regions}.${svtype}.${calls_name}.csv
+  done
 done 
