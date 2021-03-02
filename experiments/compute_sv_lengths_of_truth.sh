d009aab8f9008619179984859564b5d5344ad201
Peter McHale <peter.thomas.mchale@gmail.com>
2021-03-02 09:55:44 -0700

    sv lengths

--- experiments/compute_sv_lengths_of_truth.sh
+++ experiments/compute_sv_lengths_of_truth.sh
@@ -20,7 +20,6 @@ PS4='+ (${BASH_SOURCE[0]##*/} @ ${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 for regions in all genes exons_UTRs; do
   if [[ ${regions} == "all" ]]; then 
     regions="INS"
-    exit 1
   fi
   output="/scratch/ucgd/lustre-work/quinlan/u6018199/chaisson_2019/analysis/locally_assemble_short_reads/trfermikit/experiments/${regions}/singleBaseMatchReward_singleBaseMismatchPenalty_gapOpenPenalties_gapExtensionPenalties/data/gapExtensionPenalties=1,0_gapOpenPenalties=16,41_singleBaseMatchReward=10_singleBaseMismatchPenalty=12"
   bash compute_sv_lengths_of_truth_on_regions.sh --output ${output}
