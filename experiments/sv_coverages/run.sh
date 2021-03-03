8dcb4c4497904cd22315cb49cb6dfad152504b40
Peter McHale <peter.thomas.mchale@gmail.com>
2021-03-03 13:05:49 -0700

    sv coverage scripts

--- experiments/sv_coverages/run.sh
+++ experiments/sv_coverages/run.sh
@@ -17,6 +17,6 @@ set -o xtrace
 # https://www.gnu.org/software/bash/manual/html_node/Bash-Variables.html
 PS4='+ (${BASH_SOURCE[0]##*/} @ ${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 
-# bash compute_sv_coverages.sh --alignments-name "short_reads"
-bash compute_sv_coverages.sh --alignments-name "unitigs"
+bash compute_sv_coverages.sh --alignments-name "short_reads"
+# bash compute_sv_coverages.sh --alignments-name "unitigs"
   
